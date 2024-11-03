9a34e45263bb55b18df3028e4fc103f80397cc5f
j1j23 <jaishilxlri@gmail.com>
2024-11-03 17:55:30 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-gradient-to-br from-indigo-100 via-purple-50 to-pink-100`}>{children}</body>
     </html>
   )
 }
